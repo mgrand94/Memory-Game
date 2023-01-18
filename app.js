f4e75439a6ef31d0f7aeb90be7a352d6541e6b71
Your Name <Marcosemail2015@gmail.com>
2023-01-18 14:51:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const cardArray = [
+    {
+        name: 'tesla',
+        img: 'images/tesla.jpg',
+    },
+    {
+        name: 'ford',
+        img: "images/ford.jpg",
+    }
+    ,
+    {
+        name: 'jeep',
+        img: "images/ford.jpg",
+    }
+    ,
+    {
+        name: 'porsche',
+        img: "images/porsche.jpg",
+    },
+    {
+        name: 'toyota',
+        img: "images/toyota.jpg",
+    },
+    {
+        name: 'tesla',
+        img: 'images/tesla.jpg',
+    },
+    {
+        name: 'ford',
+        img: "images/ford.jpg",
+    }
+    ,
+    {
+        name: 'jeep',
+        img: "images/jeep.jpg",
+    }
+    ,
+    {
+        name: 'porsche',
+        img: "images/porsche.jpg",
+    },
+    {
+        name: 'toyota',
+        img: 'images/toyota.jpg',
+    }
+    ,
+    {
+        name: 'benz',
+        img: 'images/benz.jpg',
+    }
+    ,
+    {
+        name: 'benz',
+        img: 'images/benz.jpg',
+    }
+]
+
+//shuffling array randomly
+//cardArray.sort(() => 0.5 - Math.random())
+
+console.log(cardArray);
